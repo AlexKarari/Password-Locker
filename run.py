f9bbcbe5292d32c7f194e3a513385908ec8aa6a6
alexkarari <k.alexander@gmail.com>
2018-03-31 17:37:19 +0300

    run.py
    Account creation and log in commands created.

--- run.py
+++ run.py
@@ -45,4 +45,57 @@ def display_credentials():
     '''
     return User_Credentials.display_credentials()
 
+def main():
+    while True:
+        print("Welcome to password Locker Application.")
+        print('\n')
+        print("Use these short codes to select an option: Create New User account use 'cu': Login to your account use 'lg' or 'esc' to exit password locker")
+        short_code = input().lower()
+        print('\n')
+
+        if short_code == 'cc':
+            print("Create your prefered username")
+            print("-"*15)
+            new_username = input()
+
+            print("Create a desired password")
+            print("-"*15)
+            new_password = input()
+
+            print("Confirm password")
+            confirm_password = input()
+
+            while confirm_password != new_password:
+                print("Sorry, passwords inputted did not match! Try again.")
+                print("Enter a password")
+                new_password = input()
+                print("Confirm Your Password")
+                confirm_password = input()
+            else:
+                print(f"Congratulations {new_username}! You have created your new account.")
+                print("-"*20)
+                print('\n')
+                print("Proceed to your Account")
+                print("Username:")
+                created_username = input()
+                print("Password:")
+                created_password = input()
+
+                while created_username != new_username or created_password != new_password:
+                    print("You entered a wrong username or password")
+                    print("Username:")
+                    created_username = input()
+                    print("Your Password")
+                    created_password = input()
+                else:
+                    print(f"Welcome: {created_username} to your Account")
+                    print('\n')
+
+                   
+
+
+
+
+
+
 
